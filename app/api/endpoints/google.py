e2334f006d1f9708f002bb33edaa02948cf7b1dd
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-02-22 13:39:47 +0300

    Rev2 added patch for the tests passing and returned ref for spreadsheet

--- app/api/endpoints/google.py
+++ app/api/endpoints/google.py
@@ -18,35 +18,3 @@ async def get_report(
     """Только для суперюзеров."""
     projects = await charity_crud.get_projects_by_completion_rate(session)
     return await upload(projects, wrapper_services)
-
-
-'''
-from typing import List
-
-from aiogoogle import Aiogoogle
-from fastapi import APIRouter, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from app import schemas
-from app.core import current_superuser, get_async_session
-from app.crud import charity_crud
-from app.google_services import get_google_service, upload
-
-
-router = APIRouter(prefix='/google', tags=['Google'])
-
-
-@router.post(
-    '/',
-    response_model=List[schemas.CharityResponse],
-    dependencies=[Depends(current_superuser)],
-)
-async def get_report(
-        session: AsyncSession = Depends(get_async_session),
-        wrapper_services: Aiogoogle = Depends(get_google_service),
-):
-    """Только для суперюзеров."""
-    projects = await charity_crud.get_projects_by_completion_rate(session)
-    await upload(projects, wrapper_services)
-    return projects
-'''
