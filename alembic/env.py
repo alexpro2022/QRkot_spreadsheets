95d7622835c1978dec4cfda8f2e20dde70d211a2
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-04-14 19:41:30 +0300

    Set direct DB address in alembic env.py

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,5 @@
 import asyncio
-import os
+# import os
 from logging.config import fileConfig
 
 from dotenv import load_dotenv
@@ -13,7 +13,8 @@ load_dotenv('.env')
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option('sqlalchemy.url', os.environ['DATABASE_URL'])
+# config.set_main_option('sqlalchemy.url', os.environ['DATABASE_URL'])
+config.set_main_option('sqlalchemy.url', 'postgresql+asyncpg://postgres:postgres@db:5432/postgres')
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
